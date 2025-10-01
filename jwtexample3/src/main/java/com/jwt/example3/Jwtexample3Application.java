5e84cbade6ae31a218947186fb5353b5e7c178c6
Gourav Raj <zhackerloopback@gmail.com>
2025-10-01 20:06:12 +0530

    Oauth2 security
    GitHub OAuth2 in a Spring project enables secure user authentication via GitHub accounts. Spring Security handles the OAuth2 flow, retrieving user details from GitHub and managing login sessions—ideal for integrating social login with minimal setup.

---
+++ jwtexample3/src/main/java/com/jwt/example3/Jwtexample3Application.java
@@ -0,0 +1,13 @@
+package com.jwt.example3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwtexample3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwtexample3Application.class, args);
+	}
+
+}
---
+++ jwtexample3/src/main/java/com/jwt/example3/controllers/models/User.java
@@ -0,0 +1,15 @@
+package com.jwt.example3.controllers.models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+    private String userId;
+    private String name;
+    private String email;
+}
